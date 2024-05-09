9bae0df882de5532f542fd69de359c75e6b1baf2
jeremiahmark <jeremiah.o.mark@outlook.com>
2024-05-08 18:57:23 -0700

    test

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,6 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
-  let ga_id = 'G-RRCZW6YW84';
-
-  document.write(`<script async src="https://www.googletagmanager.com/gtag/js?id=${ga_id}"></script>`);
-  const script1 = document.createElement('script');
-  script1.innerHTML = `window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', '${ga_id}');`;
-  document.head.appendChild(script1);
-
   enableProdMode();
 }
 

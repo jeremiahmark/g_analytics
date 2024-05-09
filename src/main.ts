bd7ba411d07eabc7ad99e40bcb7e616ba5e5d936
jeremiahmark <jeremiah.o.mark@outlook.com>
2024-05-08 18:41:14 -0700

    test

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,20 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
+  // let ga_id = 'G-RRCZW6YW84';
+
+  // document.write(`<script async src="https://www.googletagmanager.com/gtag/js?id=${ga_id}"></script>`);
+  // document.write(`
+  //   <script>
+  //     window.dataLayer = window.dataLayer || [];
+  //     function gtag(){dataLayer.push(arguments);}
+  //     gtag('js', new Date());
+  
+  //     gtag('config', '${ga_id}');
+  //   </script>
+  // `);
+
+
   enableProdMode();
 }
 
